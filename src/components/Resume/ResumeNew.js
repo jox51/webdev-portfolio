5890715f5169f8762f201536b0d63045b0100681
Orlando L Dominici <orlandoldominici@Orlandos-Air.home>
2023-02-28 19:31:57 -0800

    fixed spelling errors in pdf url

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -10,7 +10,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css"
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`
 
 const resumeLink =
-  "https://raw.githubusercontent.com/jox51/webdev-portfolio/main/src/Assets/WebDev_Resume_Dominici.pdf"
+  "https://raw.githubusercontent.com/jox51/webdev-portfolio/main/src/Assets/WebDev-Resume-Dominici.pdf"
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200)
