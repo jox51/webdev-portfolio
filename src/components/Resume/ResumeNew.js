70c9ef8ec43188a5e07ba475b9b3ff77da5310ea
Orlando L Dominici <orlandoldominici@Orlandos-MacBook-Air.local>
2023-02-28 17:13:24 -0800

    added images

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -10,6 +10,9 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css"
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`
 
 const resumeLink =
+  "https://github.com/jox51/webdev-portfolio/blob/main/src/Assets/WebDev_Resume_Dominici.pdf"
+
+const resumeLink2 =
   "https://raw.githubusercontent.com/soumyajit4419/portfolio/master/src/Assets/Soumyajit_Behera-BIT_MESRA.pdf"
 
 function ResumeNew() {
@@ -36,7 +39,10 @@ function ResumeNew() {
         </Row>
 
         <Row className="resume">
-          <Document file={resumeLink} className="d-flex justify-content-center">
+          <Document
+            file={`https://cors-anywhere.herokuapp.com/${resumeLink}`}
+            className="d-flex justify-content-center"
+          >
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
